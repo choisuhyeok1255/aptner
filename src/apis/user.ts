f95ed74441abd3fe4b70c222de59acbb76ce3b6a
choisuhyeok1255 <csh300029@gmail.com>
2024-10-26 21:32:18 +0900

    feat: getUserFollowersAPI, getUserSubscriptionsAPI, getUserOrganizationsAPI 추가 및 타입 추가

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,10 +1,50 @@
 import { ax } from "@/apis";
-import type { GetUsersQueryModel, GetUsersServerModel } from "@/types";
+import type {
+  GetUserFollowersQueryModel,
+  GetUserFollowersServerModelType,
+  GetUserOrganizationsQueryModel,
+  GetUserOrganizationsServerModelType,
+  GetUsersQueryModel,
+  GetUsersServerModel,
+  GetUserSubscriptionsQueryModel,
+  GetUserSubscriptionsServerModelType,
+} from "@/types";
 
 export const getUsersAPI = async (req: GetUsersQueryModel) => {
   const { data } = await ax.get<GetUsersServerModel>(`search/users`, {
     params: { q: req.query.username, page: req.query.page, per_page: 20 },
   });
+
+  return data;
+};
+
+export const getUserFollowersAPI = async (req: GetUserFollowersQueryModel) => {
+  const { data } = await ax.get<GetUserFollowersServerModelType>(
+    `users/${req.query.username}/followers`,
+    { params: { per_page: 100 } }
+  );
+
+  return data;
+};
+
+export const getUserSubscriptionsAPI = async (
+  req: GetUserSubscriptionsQueryModel
+) => {
+  const { data } = await ax.get<GetUserSubscriptionsServerModelType>(
+    `users/${req.query.username}/subscriptions`,
+    { params: { per_page: 100 } }
+  );
+
+  return data;
+};
+
+export const getUserOrganizationsAPI = async (
+  req: GetUserOrganizationsQueryModel
+) => {
+  const { data } = await ax.get<GetUserOrganizationsServerModelType>(
+    `users/${req.query.username}/orgs`,
+    { params: { per_page: 100 } }
+  );
 
   return data;
 };
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,16 +1,68 @@
-import { getUsersAPI } from "@/apis";
-import { GetUsersQueryModel } from "@/types";
+import {
+  getUserFollowersAPI,
+  getUserOrganizationsAPI,
+  getUsersAPI,
+  getUserSubscriptionsAPI,
+} from "@/apis";
+import {
+  GetUserFollowersQueryModel,
+  GetUserOrganizationsQueryModel,
+  GetUsersQueryModel,
+  GetUserSubscriptionsQueryModel,
+} from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
 const userKeys = {
   all: ["user"] as const,
   users: () => [...userKeys.all, "user"] as const,
   user: (req: GetUsersQueryModel) => [...userKeys.users(), req] as const,
+  followers: () => [...userKeys.all, "followers"] as const,
+  follower: (req: GetUserFollowersQueryModel) =>
+    [...userKeys.followers(), req] as const,
+  subscriptions: () => [...userKeys.all, "subscription"] as const,
+  subscription: (req: GetUserSubscriptionsQueryModel) =>
+    [...userKeys.subscriptions(), req] as const,
+  Organizations: () => [...userKeys.all, "organization"] as const,
+  Organization: (req: GetUserOrganizationsQueryModel) =>
+    [...userKeys.Organizations(), req] as const,
 };
 
 export const useGetUsers = (req: GetUsersQueryModel) => {
   return useQuery({
     queryKey: userKeys.user(req),
     queryFn: () => getUsersAPI(req),
+  });
+};
+
+export const useGetUserFollowers = (
+  req: GetUserFollowersQueryModel,
+  enabled: boolean
+) => {
+  return useQuery({
+    queryKey: userKeys.follower(req),
+    queryFn: () => getUserFollowersAPI(req),
+    enabled,
+  });
+};
+
+export const useGetUserSubscriptions = (
+  req: GetUserSubscriptionsQueryModel,
+  enabled: boolean
+) => {
+  return useQuery({
+    queryKey: userKeys.subscription(req),
+    queryFn: () => getUserSubscriptionsAPI(req),
+    enabled,
+  });
+};
+
+export const useGetUserOrganizations = (
+  req: GetUserOrganizationsQueryModel,
+  enabled: boolean
+) => {
+  return useQuery({
+    queryKey: userKeys.Organization(req),
+    queryFn: () => getUserOrganizationsAPI(req),
+    enabled,
   });
 };
