ad8718cc3ba3470d90e0140c80d374615051435b
choisuhyeok1255 <csh300029@gmail.com>
2024-10-27 18:19:49 +0900

    feat: GithubProfileSkeletion 컴포넌트 추가

---
+++ src/components/githubProfileList/containers/githubProfile/GithubProfile.skeleton.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+import * as S from "./GithubProfile.styled";
+
+interface GithubProfileSkeletonProps {
+  skeletonCount: number;
+  contents: readonly string[];
+}
+
+const GithubProfileSkeleton = ({
+  skeletonCount,
+  contents,
+}: GithubProfileSkeletonProps) => {
+  return Array.from({ length: skeletonCount }).map((_, i) => (
+    <S.GithubProfile key={i}>
+      <Skeleton css={S.profile} width={75} height={75} />
+      <S.ContentWarpper>
+        <Skeleton width={200} />
+        {contents.map((_, i) => (
+          <Skeleton key={i} width={85} />
+        ))}
+      </S.ContentWarpper>
+      <Skeleton css={S.bookmarkButton} width={24} height={24} />
+    </S.GithubProfile>
+  ));
+};
+
+export default GithubProfileSkeleton;
--- src/components/githubProfileList/containers/index.ts
+++ src/components/githubProfileList/containers/index.ts
@@ -1,1 +1,2 @@
 export { default as GithubProfile } from "./githubProfile/GithubProfile";
+export { default as GithubProfileSkeleton } from "./githubProfile/GithubProfile.skeleton";
