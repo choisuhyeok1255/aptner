e239bcb6ebc0ba99843a2bf1e282a2e09ae327dd
choisuhyeok1255 <csh300029@gmail.com>
2024-10-27 15:15:46 +0900

    feat: GithubProfile 무한스크롤로 인해 forwardRef 추가, Content Click Button label 수정

--- src/components/githubProfileList/containers/githubProfile/GithubProfile.tsx
+++ src/components/githubProfileList/containers/githubProfile/GithubProfile.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { forwardRef, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { capitalize } from "lodash-es";
@@ -22,85 +22,84 @@ interface GithubProfileProps {
 
 const CONTENT_NAMES = ["followers", "organizations", "subscriptions"] as const;
 
-const GithubProfile = ({
-  name,
-  githubUrl,
-  profileUrl,
-  isBookmark,
-}: GithubProfileProps) => {
-  const [content, setContent] = useState<
-    Record<(typeof CONTENT_NAMES)[number], boolean>
-  >({
-    followers: false,
-    organizations: false,
-    subscriptions: false,
-  });
+const GithubProfile = forwardRef<HTMLLIElement, GithubProfileProps>(
+  ({ name, githubUrl, profileUrl, isBookmark }: GithubProfileProps, ref) => {
+    const [content, setContent] = useState<
+      Record<(typeof CONTENT_NAMES)[number], boolean>
+    >({
+      followers: false,
+      organizations: false,
+      subscriptions: false,
+    });
 
-  const { data: followers } = useGetUserFollowers(
-    { query: { username: name } },
-    content.followers
-  );
-  const { data: subscriptions } = useGetUserSubscriptions(
-    { query: { username: name } },
-    content.subscriptions
-  );
-  const { data: organizations } = useGetUserOrganizations(
-    { query: { username: name } },
-    content.organizations
-  );
+    const { data: followers } = useGetUserFollowers(
+      { query: { username: name } },
+      content.followers
+    );
+    const { data: subscriptions } = useGetUserSubscriptions(
+      { query: { username: name } },
+      content.subscriptions
+    );
+    const { data: organizations } = useGetUserOrganizations(
+      { query: { username: name } },
+      content.organizations
+    );
 
-  const handleBookmark = (): void => {
-    console.log("handleBookmark");
-  };
-
-  const handleSearchContent =
-    (contentKey: (typeof CONTENT_NAMES)[number]) => (): void => {
-      setContent((prevContent) => ({ ...prevContent, [contentKey]: true }));
+    const handleBookmark = (): void => {
+      console.log("handleBookmark");
     };
 
-  return (
-    <S.GithubProfile>
-      {profileUrl && (
-        <Image
-          css={S.profile}
-          width={75}
-          height={75}
-          src={profileUrl}
-          alt={`${name}님의 프로필`}
-          priority
-        />
-      )}
-      <S.ContentWarpper>
-        <Link css={S.userName} href={githubUrl} target="_blank">
-          {name}
-        </Link>
-        {CONTENT_NAMES.map((contentName) => (
-          <S.Content key={contentName}>
-            <S.ContentName>{`${capitalize(contentName)}`}:</S.ContentName>
-            {content[contentName] ? (
-              <S.Count>
-                {contentName === "followers"
-                  ? followers?.length
-                  : contentName === "subscriptions"
-                  ? subscriptions?.length
-                  : organizations?.length}
-              </S.Count>
-            ) : (
-              <S.ClickButton
-                type="button"
-                onClick={handleSearchContent(contentName)}
-              >
-                Click me
-              </S.ClickButton>
-            )}
-          </S.Content>
-        ))}
-        <S.BookmarkButton type="button" onClick={handleBookmark}>
-          {isBookmark ? <HeartFillIcon /> : <HeartEmptyIcon />}
-        </S.BookmarkButton>
-      </S.ContentWarpper>
-    </S.GithubProfile>
-  );
-};
+    const handleSearchContent =
+      (contentKey: (typeof CONTENT_NAMES)[number]) => (): void => {
+        setContent((prevContent) => ({ ...prevContent, [contentKey]: true }));
+      };
+
+    return (
+      <S.GithubProfile ref={ref}>
+        {profileUrl && (
+          <Image
+            css={S.profile}
+            width={75}
+            height={75}
+            src={profileUrl}
+            alt={`${name}님의 프로필`}
+            priority
+          />
+        )}
+        <S.ContentWarpper>
+          <Link css={S.userName} href={githubUrl} target="_blank">
+            {name}
+          </Link>
+          {CONTENT_NAMES.map((contentName) => (
+            <S.Content key={contentName}>
+              <S.ContentName>{`${capitalize(contentName)}`}:</S.ContentName>
+              {content[contentName] ? (
+                <S.Count>
+                  {contentName === "followers"
+                    ? followers?.length
+                    : contentName === "subscriptions"
+                    ? subscriptions?.length
+                    : organizations?.length}
+                </S.Count>
+              ) : (
+                <S.ClickButton
+                  type="button"
+                  onClick={handleSearchContent(contentName)}
+                >
+                  Check
+                </S.ClickButton>
+              )}
+            </S.Content>
+          ))}
+          <S.BookmarkButton type="button" onClick={handleBookmark}>
+            {isBookmark ? <HeartFillIcon /> : <HeartEmptyIcon />}
+          </S.BookmarkButton>
+        </S.ContentWarpper>
+      </S.GithubProfile>
+    );
+  }
+);
+
+GithubProfile.displayName = "GithubProfile";
 
 export default GithubProfile;
