1bd56f428a0e633147d27eebcedcf902629f916d
choisuhyeok1255 <csh300029@gmail.com>
2024-10-28 00:55:39 +0900

    feat: ax에서 interceptors사용으로 api 호출 에러시 alert으로 메세지 보여주는 기능 추가

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -2,4 +2,11 @@ import axios from "axios";
 
 const ax = axios.create({ baseURL: "https://api.github.com/" });
 
+ax.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    alert(error.response.data.message);
+  }
+);
+
 export default ax;
