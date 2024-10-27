512fbace6fd7b5f1db9181fcf98e4bcc5e6a54c4
choisuhyeok1255 <csh300029@gmail.com>
2024-10-27 23:29:43 +0900

    feat: 루트페이지 유저 검색 기능 구현 및 간단한 css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Home</div>;
-}
+"use client";
+
+import { SearchGithubProfile } from "@/components";
+import * as S from "./page.styled";
+
+const Home = () => {
+  return (
+    <S.Section>
+      <S.Heading>깃허브 유저 검색</S.Heading>
+      <SearchGithubProfile />
+    </S.Section>
+  );
+};
+
+export default Home;
