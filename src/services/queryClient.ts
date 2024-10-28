4849b42cbb99011b0692ff3d8f20043cdebafb49
choisuhyeok1255 <csh300029@gmail.com>
2024-10-28 11:06:52 +0900

    feat: queryClient retry, refetchOnMount, refetchOnWindowFocus 옵션 해제

--- src/services/queryClient.ts
+++ src/services/queryClient.ts
@@ -1,5 +1,13 @@
 import { QueryClient } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default queryClient;
