b73e7953b8a9a66f956c81af0f44b3ce69d462f7
choisuhyeok1255 <csh300029@gmail.com>
2024-10-28 10:32:21 +0900

    style: Input 컴포넌트 border 제거, 그림자 효과 추가

--- src/components/common/input/Input.styled.ts
+++ src/components/common/input/Input.styled.ts
@@ -3,8 +3,9 @@ import styled from "@emotion/styled";
 export const Input = styled.input`
   width: 200px;
   height: 50px;
-  border: 1px solid #8d94a0;
+  border: 0;
   border-radius: 10px;
   padding: 10px;
   font-size: 16px;
+  box-shadow: 0px 4px 14px 6px #97979726;
 `;
