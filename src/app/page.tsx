6179df9e231f53992753bd0107de33be1a8446ec
choisuhyeok1255 <csh300029@gmail.com>
2024-10-28 11:31:40 +0900

    feat: SearchGithubProfile ->  useSearchParams 사용을 위해 Suspense 추가, use client 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@
 
 import { SearchGithubProfile } from "@/components";
 import * as S from "./page.styled";
+import { Suspense } from "react";
 
 const Home = () => {
   return (
     <S.Section>
       <S.Heading>깃허브 유저 검색</S.Heading>
-      <SearchGithubProfile css={S.searchGithubProfile} />
+      <Suspense>
+        <SearchGithubProfile css={S.searchGithubProfile} />
+      </Suspense>
     </S.Section>
   );
 };
--- src/components/githubProfile/searchGithubProfile/SearchGithubProfile.tsx
+++ src/components/githubProfile/searchGithubProfile/SearchGithubProfile.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 import { GithubProfileCardSkeleton, GithubProfiles, Input } from "@/components";
