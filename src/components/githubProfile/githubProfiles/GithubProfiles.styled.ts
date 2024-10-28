4fbfa8b53f936977a2ea6f7c41605d83d8d7de94
choisuhyeok1255 <csh300029@gmail.com>
2024-10-28 10:33:51 +0900

    style: GithubProfiles 스크롤 효과 추가

--- src/components/githubProfile/githubProfiles/GithubProfiles.styled.ts
+++ src/components/githubProfile/githubProfiles/GithubProfiles.styled.ts
@@ -3,8 +3,12 @@ import styled from "@emotion/styled";
 export const GithubProfileWrapper = styled.ul`
   display: flex;
   flex-flow: column;
+  align-items: center;
   row-gap: 10px;
+  width: 100%;
   margin-bottom: 10px;
+  padding: 10px;
+  overflow: auto;
 `;
 
 export const EmptyList = styled.li`
