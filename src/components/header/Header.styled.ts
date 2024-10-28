cea7dbece793881f837326c624288a0f8d09efa9
choisuhyeok1255 <csh300029@gmail.com>
2024-10-28 11:05:12 +0900

    style: Header height 상수로 관리 및 적용

--- src/components/header/Header.styled.ts
+++ src/components/header/Header.styled.ts
@@ -1,10 +1,13 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
+import { HEADER_HEIGHT } from "@/constants";
+
 export const LinkWrapper = styled.nav`
   display: flex;
   justify-content: center;
   column-gap: 20px;
+  height: ${HEADER_HEIGHT};
   padding: 15px;
 `;
 
