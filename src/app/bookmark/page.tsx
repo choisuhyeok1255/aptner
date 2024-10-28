3e66e9170acac455b1691775f5ee2a3b478dd13a
choisuhyeok1255 <csh300029@gmail.com>
2024-10-28 12:34:08 +0900

    feat: 북마크페이지, 헤더에서 즐겨찾기 -> 북마크로 단어 변경

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -6,7 +6,7 @@ import * as S from "./page.styled";
 const Bookmark = () => {
   return (
     <S.Section>
-      <S.Heading>즐겨찾기 리스트</S.Heading>
+      <S.Heading>북마크 리스트</S.Heading>
       <GithubProfileBookmarks />
     </S.Section>
   );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,7 +12,9 @@ const Header = () => {
 
   return (
     <header>
-      <h1 className="a11y">깃허브 유저 검색 및 즐겨찾기</h1>
+      <h1 className="a11y">
+        깃허브 유저 검색 및 유저 북마크 리스트 관리 페이지
+      </h1>
       <S.LinkWrapper aria-label="헤더 내비게이션">
         <Link
           css={S.link(isCurrentPath(PATH.root))}
@@ -26,7 +28,7 @@ const Header = () => {
           href={PATH.bookmark}
           aria-current={isCurrentPath(PATH.bookmark) ? "page" : undefined}
         >
-          유저 즐겨찾기
+          북마크
         </Link>
       </S.LinkWrapper>
     </header>
